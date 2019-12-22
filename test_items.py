9e037f3abcfd50ca6a62bf85bbb1f90c3dc3de45
Viktor32-sours <52598245+Viktor32-sours@users.noreply.github.com>
2019-12-22 18:14:23 +0400

    Add new files language test
    Добавлены файлы для проверки в language test

---
+++ test_items.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_mast_be_button_add_basket_pass(browser):
+    browser.get(link)
+    time.sleep(30)
+    
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert button, "Кнопки 'Добавить' нет на месте"
+
+
